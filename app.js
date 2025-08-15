6ed12624c693e45dabb85aafc5800eb8e6997c06
ChristianJair27 <99058213+ChristianJair27@users.noreply.github.com>
2025-08-14 22:00:35 -0600

    Update app.js

--- app.js
+++ app.js
@@ -41,6 +41,8 @@ app.use(cookieParser());
       }
     },
     credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   }));
 
 app.use(express.json());
