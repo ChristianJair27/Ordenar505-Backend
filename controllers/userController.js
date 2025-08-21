35be4df98f9f01fea424b835939bcd09e494228b
Minecon27 <christianjair27@gmail.com>
2025-08-21 00:35:55 -0600

    fix delete user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,115 +1,3 @@
- /*const createHttpError = require("http-errors");
-const User = require("../models/userModel");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-const config = require("../config/config");
-
-const register = async (req, res, next) => {
-    try {
-
-        const { name, phone, email, password, role } = req.body;
-
-        if(!name || !phone || !email || !password || !role){
-            const error = createHttpError(400, "All fields are required!");
-            return next(error);
-        }
-
-        const isUserPresent = await User.findOne({email});
-        if(isUserPresent){
-            const error = createHttpError(400, "User already exist!");
-            return next(error);
-        }
-
-
-        const user = { name, phone, email, password, role };
-        const newUser = User(user);
-        await newUser.save();
-
-        res.status(201).json({success: true, message: "New user created!", data: newUser});
-
-
-    } catch (error) {
-        next(error);
-    }
-}
-
-
-const login = async (req, res, next) => {
-
-    try {
-        
-        const { email, password } = req.body;
-
-        if(!email || !password) {
-            const error = createHttpError(400, "All fields are required!");
-            return next(error);
-        }
-
-        const isUserPresent = await User.findOne({email});
-        if(!isUserPresent){
-            const error = createHttpError(401, "Invalid Credentials");
-            return next(error);
-        }
-
-        const isMatch = await bcrypt.compare(password, isUserPresent.password);
-        if(!isMatch){
-            const error = createHttpError(401, "Invalid Credentials");
-            return next(error);
-        }
-
-        const accessToken = jwt.sign({_id: isUserPresent._id}, config.accessTokenSecret, {
-            expiresIn : '1d'
-        });
-
-        res.cookie('accessToken', accessToken, {
-            maxAge: 1000 * 60 * 60 *24 * 30,
-            httpOnly: true,
-            sameSite: 'none',
-            secure: true
-        })
-
-        res.status(200).json({success: true, message: "User login successfully!", 
-            data: isUserPresent
-        });
-
-
-    } catch (error) {
-        next(error);
-    }
-
-}
-
-const getUserData = async (req, res, next) => {
-    try {
-        
-        const user = await User.findById(req.user._id);
-        res.status(200).json({success: true, data: user});
-
-    } catch (error) {
-        next(error);
-    }
-}
-
-const logout = async (req, res, next) => {
-    try {
-        
-        res.clearCookie('accessToken');
-        res.status(200).json({success: true, message: "User logout successfully!"});
-
-    } catch (error) {
-        next(error);
-    }
-}
-
-
-
-
-module.exports = { register, login, getUserData, logout }*/
-
-
-
-
-
 
 
 const createHttpError = require("http-errors");
@@ -279,18 +167,16 @@ const updateUser = async (req, res, next) => {
 
 // ELIMINAR USUARIO
 const deleteUser = async (req, res, next) => {
+  const { id } = req.params;
   try {
-    const { id } = req.params;
-
-    const [result] = await db.query("DELETE FROM users WHERE id = ?", [id]);
-
-    if (result.affectedRows === 0) {
-      return next(createHttpError(404, "User not found"));
+    const [r] = await db.query("DELETE FROM users WHERE id=?", [id]);
+    if (r.affectedRows === 0) return next(createHttpError(404, "User not found"));
+    res.json({ success: true, message: "User deleted!" });
+  } catch (e) {
+    if (e.code === "ER_ROW_IS_REFERENCED_2" || e.errno === 1451) {
+      return next(createHttpError(409, "No se puede borrar: el usuario tiene movimientos/turnos relacionados. Usa desactivación o ajusta FKs (SET NULL/CASCADE)."));
     }
-
-    res.status(200).json({ success: true, message: "User deleted!" });
-  } catch (error) {
-    next(error);
+    next(e);
   }
 };
 
