d256797cdb09f7925451c8867e624e54a3ab9599
Minecon27 <christianjair27@gmail.com>
2025-08-29 23:33:53 -0600

    fx lk

--- config/config.js
+++ config/config.js
@@ -8,10 +8,10 @@ const config = Object.freeze({
     nodeEnv: process.env.NODE_ENV || "development",
 
     // MySQL
-    dbHost: process.env.DB_HOST || "srv1250.hstgr.io",
-    dbUser: process.env.DB_USER || "u522428285_admin",
-    dbPass: process.env.DB_PASSWORD || "Minecon$2710",
-    dbName: process.env.DB_NAME || "u522428285_ordenar",
+    dbHost: process.env.DB_HOST ,
+    dbUser: process.env.DB_USER ,
+    dbPass: process.env.DB_PASSWORD ,
+    dbName: process.env.DB_NAME,
 
     // JWT
     accessTokenSecret: process.env.ACCESS_TOKEN_SECRET,
